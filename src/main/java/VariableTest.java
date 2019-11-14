186fd3c0f93c875e858f5eea3e7eca18fcfd16f2
zmy <wb-zmy629678@hz.ali.com>
2019-11-14 11:10:28 +0800

    java

--- src/main/java/VariableTest.java
+++ src/main/java/VariableTest.java
@@ -2,8 +2,10 @@
  * 变量
  */
 public class VariableTest {
-    int a = 3;
-    double b = 3.0;
-
-
+    static int sta ;//静态变量从属于类
+    int var ;//成员变量从属于对象，会给出默认的初始化值
+    public static void main(String[] args) {
+        int mem = 1;//局部变量从属于方法,必须初始化，必须赋值。
+        System.out.println(mem);
+    }
 }
